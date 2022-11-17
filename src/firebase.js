445887c2509775a6d4aa295a67f0afdedf9624c1
JunHo Kim <twinjyjh5@gmail.com>
2022-11-17 11:52:17 +0000

    ReChat SetUp Complete

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import {initializeApp} from "firebase/app";
+import firebase from 'firebase/compat/app';
+
+const firebaseKey = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+firebase.initializeApp(firebaseKey);
