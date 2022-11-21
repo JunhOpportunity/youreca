79bceaca5a86bac980a3cf2fba49a364279d8dc5
JunHo Kim <twinjyjh5@gmail.com>
2022-11-21 11:53:47 +0000

    First Login Check

--- src/Components/ReRe.js
+++ src/Components/ReRe.js
@@ -2,19 +2,38 @@ import React, { useEffect, useState } from "react";
 import { dbService } from "../firebase.js";
 import styled from "styled-components";
 import { authService } from "../firebase.js";
-
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 const Wrapper = styled.div``;
 
-/* const Input = styled.input`
+const Input = styled.input`
+  border: none;
+  border-radius: 50px;
+  background-color: beige;
+`;
+
+const TextArea = styled.textarea`
+  border: none;
+  background-color: beige;
   width: 200px;
   height: 200px;
-  border: none;
-  background-color: #696969;
-  color: white;
-`; */
-
+`;
 
 export default function ReRe({ userInfo }) {
+  const navigation = useNavigate();
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      console.log('all', user)
+      console.log('onAuth', user.email);
+      const created = user.metadata.createdAt;
+      const last = user.metadata.lastLoginAt;
+      console.log('real', created, last)
+      console.log(created === last);
+    } else {
+      navigation("/");
+    }
+  });
   const [response, SetResponse] = useState("");
 
   const onChange = (event) => {
@@ -25,12 +44,14 @@ export default function ReRe({ userInfo }) {
   };
 
   const onSubmit = async (event) => {
-    console.log(userInfo)
+    console.log(userInfo);
     event.preventDefault();
     const user = authService.currentUser;
-    await dbService
-      .collection("ReArchive")
-      .add({ response: response, userId: userInfo.userId, createdTime: Date() });
+    await dbService.collection("ReArchive").add({
+      response: response,
+      userId: userInfo.userId,
+      createdTime: Date(),
+    });
     SetResponse("");
   };
 
@@ -38,12 +59,18 @@ export default function ReRe({ userInfo }) {
     <>
       <Wrapper>
         <form onSubmit={onSubmit}>
-          <input
+          <Input
             type="text"
             placeholder="김준호라는 사람은 어떤 사람이었는지, 어떤 생활을 해왔는지 자유롭게 적어주세요!"
             value={response}
             onChange={onChange}
           />
+          <TextArea
+            type="text"
+            placeholder="김준호라는 사람은 어떤 사람이었는지, 어떤 생활을 해왔는지 자유롭게 적어주세요!"
+            value={response}
+            onChange={onChange}
+          ></TextArea>
           <input type="submit" value="게시하기" />
         </form>
       </Wrapper>
