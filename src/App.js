fa4edf8e1cb12d92d8e32f2bbea6afd5a83e7fee
JunHo Kim <twinjyjh5@gmail.com>
2022-12-10 08:11:21 +0000

    HashRouter

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import Home from "./Components/Home.js"
-import Auth from "./Components/Auth.js"
-import Profile from "./Routes/Profile.js"
-import Contact from "./Routes/Profile.js"
-import Main from "./Routes/Main.js"
-import Test from "./Routes/Test.js"
+import React from "react";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Profile from "./Routes/Profile.js";
+import Contact from "./Routes/Profile.js";
+import Main from "./Routes/Main.js";
+import Test from "./Routes/Test.js";
 import Upload from "./Routes/Upload.js";
 import FirstLogin from "./Routes/FirstLogin.js";
 import Feedback from "./Routes/Feedback.js";
 
-
 function App() {
-  const [loginCheck, setLoginCheck] = useState(false);
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Main />}/>
-          <Route path="firstlogin" element={<FirstLogin />}/>
-          <Route path="upload" element={<Upload />}/>
-          <Route path="profile" element={<Profile />}/>
-          <Route path="feedback" element={<Feedback />}/>
-          <Route path="contact" element={<Contact />}/>
-          <Route path="test" element={<Test/>}/>
+          <Route path="/" element={<Main />} />
+          <Route path="firstlogin" element={<FirstLogin />} />
+          <Route path="upload" element={<Upload />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="feedback" element={<Feedback />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="test" element={<Test />} />
         </Routes>
       </Router>
     </>
--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import firebase from "./firebase";
 import "./default.css";
-import { AnimatePresence } from "framer-motion";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
