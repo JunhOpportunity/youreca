4b791b980117d2c26d818445c45025321f667f2c
JunHo Kim <twinjyjh5@gmail.com>
2023-02-09 09:57:39 +0000

    ADD Input Image File Link Firebase

--- src/Routes/UploadProfileImage.js
+++ src/Routes/UploadProfileImage.js
@@ -2,18 +2,24 @@ import { useEffect, useRef, useState } from "react";
 import Loading from "../Components/Loading.js";
 import { storageService } from "../firebase.js";
 import { authService } from "../firebase.js";
+import styled from "styled-components";
+
+const InputCircle = styled.div``;
+
+const InputImg = styled.input`
+  display: none;
+`;
 
 export function UploadProfileImage() {
   const [attachment, setAttachment] = useState();
   const [init, setInit] = useState(false);
   const fileInput = useRef();
-  let user;
+  let user = authService.currentUser;
 
   useEffect(() => {
     authService.onAuthStateChanged(async (user) => {
       user = authService.currentUser;
       setInit(true);
-      console.log(user.uid)
     });
   }, []);
 
@@ -34,29 +40,37 @@ export function UploadProfileImage() {
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    const fileRef = await storageService.ref().child(`123/456`);
-    
+    const fileRef = storageService.ref().child(`profileImage/${user.uid}`);
     const response = await fileRef.putString(attachment, "data_url");
-    console.log(response);
+    const attachmentUrl = await response.ref.getDownloadURL();
   };
-  const onClearAttachment = () => {
-    fileInput.current.value = "";
+
+  const onClearAttachment = (e) => {
+    console.log(fileInput.current.value);
+    fileInput.current.value = null;
     setAttachment(null);
+    console.log(attachment);
   };
+
   return (
     <>
-      {" "}
       {init ? (
         <>
-          <input type="file" accept="image/*" onChange={onFileChange} />
+          <label for="image-upload">
+            Upload Image
+            <InputCircle>
+              <InputImg
+                id="image-upload"
+                type="file"
+                accept="image/*"
+                ref={fileInput}
+                onChange={onFileChange}
+              />
+            </InputCircle>
+          </label>
           {attachment && (
             <div>
-              <img
-                src={attachment}
-                ref={fileInput}
-                width="50px"
-                height="50px"
-              />
+              <img src={attachment} width="50px" height="50px" />
               <button onClick={onClearAttachment}>clear</button>
               <button onClick={onSubmit}>submit</button>
             </div>
