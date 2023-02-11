f80bc3580d877a7645c8120d842eb2ab885a5fde
JunHo Kim <twinjyjh5@gmail.com>
2023-02-11 07:16:36 +0000

    User Reputation List Styling

--- src/Components/PeopleList.js
+++ src/Components/PeopleList.js
@@ -11,8 +11,23 @@ const EmptyBox = styled.div`
   height: 50px;
 `;
 
+const Box = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const Wrapper = styled.div`
   padding: 10px;
+  gap: 10px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  @media only screen and (min-width: 768px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+  @media only screen and (min-width: 1025px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
 `;
 
 const NewPost = styled.div`
@@ -30,13 +45,11 @@ export default function RePeople() {
   const [people, setPeople] = useState([]);
   const user = authService.currentUser;
   useEffect(() => {
-    dbService
-      .collection("Person")
-      .onSnapshot((snapshot) => {
-        const peopleArray = snapshot.docs.map((doc) => ({ ...doc.data() }));
-        console.log(peopleArray);
-        setPeople(peopleArray);
-      });
+    dbService.collection("Person").onSnapshot((snapshot) => {
+      const peopleArray = snapshot.docs.map((doc) => ({ ...doc.data() }));
+      console.log(peopleArray);
+      setPeople(peopleArray);
+    });
   }, []);
 
   const navigation = useNavigate();
@@ -52,7 +65,9 @@ export default function RePeople() {
       <NewPost onClick={goCreatePerson}>내 평판 추가하러 가기</NewPost>
       <Wrapper>
         {people.map((pers) => (
-          <PersonBox personData={pers} isMine={true} />
+          <Box>
+            <PersonBox personData={pers} isMine={true} />
+          </Box>
         ))}
       </Wrapper>
     </>
