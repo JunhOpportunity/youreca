b9399195abaf9bc0ff4512243522a16491f88fbf
JunHo Kim <twinjyjh5@gmail.com>
2023-03-10 11:46:31 +0000

    Category Page TEST 1

--- src/Routes/Test.js
+++ src/Routes/Test.js
@@ -1,4 +1,12 @@
+import { useState } from "react";
 import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display:flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 50px;
+`;
 
 const SelectBox = styled.select`
   width: 200px;
@@ -9,22 +17,45 @@ const Option = styled.option`
   background-color: orange;
 `;
 
+const CheckBox = styled.input`
+
+`;
+
+const InputText = styled.input`
+  width: 100px;
+  height: 50px;
+`;
+
 export default function Test() {
+  const [category, setCategory] = useState();
   const alphaArray = ["a", "b", "c"];
+
   const onClick = (e) => {
     console.log(e.target.value);
   };
+
+  const onChangeInput = (event) => {
+    const {
+      target: { value, type },
+    } = event;
+    if(type == "checkbox") {
+      console.log("checkbox")
+    } else if(type == "text") {
+      console.log("text")
+    }
+    setCategory(value);
+    console.log(category);
+  };
   return (
-    <>
-      <SelectBox onChange={onClick}>
+    <Wrapper>
+      <SelectBox disabled onChange={onClick}>
         {alphaArray.map((a) => {
-          return (
-            <Option  value={a}>
-              a
-            </Option>
-          );
+          return <Option value={a}>{a}</Option>;
         })}
       </SelectBox>
-    </>
+      <label for="checkbox">직접 입력하기</label>
+      <CheckBox id="checkbox" onChange={onChangeInput} type="checkbox"/>
+      <InputText value={category} onChange={onChangeInput} type="text" />
+    </Wrapper>
   );
 }
